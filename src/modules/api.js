bcacb7d2ddba5d0fab5d6dfd265ba889666e4231
shaheer ch <shaheer.chry@gmail.com>
2022-06-23 16:23:00 +0500

    Mmilestone 1

---
+++ src/modules/api.js
@@ -0,0 +1,25 @@
+const reqScoreURL =
+  'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/jgweTJ0igN7L9TDWGyrr/scores/';
+const ScoreURL = new Request(reqScoreURL);
+
+const loadLeaderBoard = async (callBack) => {
+  const response = await fetch(ScoreURL);
+  const leaderBoard = await response.json();
+  callBack(leaderBoard.result);
+};
+
+const postNewScore = async (scoreData) => {
+  fetch(ScoreURL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(scoreData),
+  })
+    .then((response) => response.json())
+    .then((score) => {
+      console.log('New score added:', score);
+    });
+};
+
+export { loadLeaderBoard, postNewScore };
